f9525729abf9ffc1c1f96e1a5d439a3dfcea1a4c
andreibalau <balauandrei28@gmail.com>
2023-11-29 18:12:46 +0200

    refactor tabs to spaces

--- src/main/java/com/rest/api/reminders/RemindersApplication.java
+++ src/main/java/com/rest/api/reminders/RemindersApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RemindersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RemindersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RemindersApplication.class, args);
+    }
 
 }
